f8c438468daab85f3ed3076452e5ed4858201828
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-12-17 14:35:51 -0600

    Updating template

--- templates/src/Home.js
+++ templates/src/Home.js
@@ -28,13 +28,6 @@ class Home extends React.Component
             <div>
                 <Navigation />
                 <div className="jumbotron p-4">
-                    {this.state.configured === false && 
-                    <p className="alert alert-danger" role="alert">
-                            <h1>This starter app requires a FREE MeshyDB account.</h1>
-                            Please create an account at <a href="https://meshydb.com" rel="noopener noreferrer" target="_blank">meshydb.com</a> and replace the values for <code>const account</code> and <code>const publicKey</code> on <code>App.js</code>.<br /><br />
-                            For details on how to get these values from your existing account please see our <a href="https://docs.meshydb.com/en/latest/intro/getting_started_nodejs.html#nodejs" rel="noopener noreferrer" target="_blank">documentation</a>
-                    </p>
-                    }
                     <h1 className="display-4">Welcome to the MeshyDB React App</h1>
                     <p className="lead">
                         This application is intended to demonstrate the MeshyDB API using the React framework. 
